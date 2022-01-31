3e254b90fdf981d67559cebcd0d16dd6f826e739
Renan Braga <renan.braga@clina.care>
2022-01-31 15:45:15 -0200

    feat(laboratory): creates cauldron

---
+++ src/app/components/laboratory/cauldron/cauldron.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cauldron',
+  templateUrl: './cauldron.component.html',
+  styleUrls: ['./cauldron.component.scss']
+})
+export class CauldronComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
